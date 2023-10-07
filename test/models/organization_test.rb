d61d871cd04f390390036879627d67071105edd0
Gitta <mail@gvanderpol.nl>
2023-10-07 12:55:21 +0200

    Add spec for organization model

--- test/models/organization_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class OrganizationTest < ActiveSupport::TestCase
-  context "associations" do
-    should have_many(:staff_accounts)
-    should have_many(:users).through(:staff_accounts)
-    should have_many(:pets)
-  end
-end
